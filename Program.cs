f34a88da0540147f513f1b4251d9cf1a5adef12e
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-30 11:30:16 -0600

    exercise setup completed

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GarysWholesomeGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tesla myTesla = new Tesla();
+            // myTesla.MaximumOccupancy = "100";
+            // Console.WriteLine(myTesla.MaximumOccupancy);
+        
+            Ram myRam = new Ram();
+            Cessna myCessna = new Cessna();
+            Zero myZero = new Zero();
+
+            myTesla.Drive();
+            myRam.Drive();
+            myCessna.Drive();
+            myZero.Drive();
+
+        }
+    }
+}
